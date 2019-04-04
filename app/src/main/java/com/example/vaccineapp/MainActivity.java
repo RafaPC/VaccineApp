b5b884aa18b7363dc13a1c68634a1387b1bb6522
jclumbreras93 <kilydiaz1@gmail.com>
2019-04-04 11:27:44 +0100

    Add bottom Bar Id

--- app/src/main/java/com/example/vaccineapp/MainActivity.java
+++ app/src/main/java/com/example/vaccineapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.vaccineapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -11,15 +14,36 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnFindDocto, btnHospital, btnDoctriod, btnAppointment, btnEmergency, btnMedicalShop;
-    ImageButton btnFindDoctor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradiant_bar));
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(itemSelect);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener itemSelect= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.nav_home:
+                    Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.nav_profile:
+                    Toast.makeText(getApplicationContext(),"Profile",Toast.LENGTH_LONG).show();
+
+                    break;
+                case R.id.nav_settings:
+                    Toast.makeText(getApplicationContext(),"Setting",Toast.LENGTH_LONG).show();
+
+                    break;
+            }
+            return false;
+        }
+    };
 
     public void changeToFindDoctors(View v){
         Toast.makeText(getApplicationContext(),"Change to 'Find Doctors' screen",Toast.LENGTH_LONG).show();
